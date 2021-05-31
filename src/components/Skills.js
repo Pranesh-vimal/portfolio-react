2487e7a8dda425886f8f69bcb2c395d3dbbc1eed
Pranesh-vimal <pranesh.vimal5@gmail.com>
2021-05-31 18:00:13 +0530

    skills added

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function Skills() {
-    return <div>Skills</div>;
+    return (
+        <div className="container mx-auto md:px-5 my-2 md:my-5">
+            <div className="text-center">
+                <h1 className="font-extrabold py-2 text-2xl md:text-4xl dark:text-white">
+                    Skills
+                </h1>
+                <p className="font-semibold py-2 dark:text-white">
+                    My technical level
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default Skills;
