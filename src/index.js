3d82aa3f950a88a2a8f790725728a326637977a8
Pranesh-vimal <pranesh.vimal5@gmail.com>
2021-05-31 17:59:02 +0530

    aos included in index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init({
+    duration: 2000,
+});
 
 ReactDOM.render(
     <React.StrictMode>
