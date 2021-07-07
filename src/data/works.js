9ae7135c1dd99a935be5ffeeed0d6a66af4832dd
Pranesh-vimal <pranesh.vimal5@gmail.com>
2021-07-07 14:40:44 +0530

    works added

---
+++ src/data/works.js
@@ -0,0 +1,79 @@
+import coronatracker from "../assets/img/corona-tracker.png";
+import seriesFinder from "../assets/img/react-series-finder.png";
+import passgen from "../assets/img/passgen-react.png";
+import pokemonApp from "../assets/img/pokemon.png";
+import spot from "../assets/img/spot.png";
+import royalcakes from "../assets/img/royal-cakes.png";
+import sayaboutme from "../assets/img/sayaboutme.png";
+import omdb from "../assets/img/omdb.png";
+import tictactoe from "../assets/img/ticktactoe.png";
+
+function works() {
+    return [
+        {
+            name: "Corona Tracker India",
+            desc: "A website to display the live corona count in India which is build using <b> Reactjs</b>.",
+            image: coronatracker,
+            website: "https://coronatrackerindia.netlify.app/",
+            github: "https://github.com/Pranesh-vimal/corona-tracker/",
+        },
+        {
+            name: "Series Finder",
+            desc: "A website to display the series details. It is developed using <b> Reactjs</b>.",
+            image: seriesFinder,
+            website: "https://pranesh-series-finder.netlify.app/",
+            github: "https://github.com/Pranesh-vimal/react-series-finder/",
+        },
+        {
+            name: "Password Generator",
+            desc: "A website to generate random password. It is developed using <b> Reactjs</b>.",
+            image: passgen,
+            website: "https://pranesh-passgen.netlify.app/",
+            github: "https://github.com/Pranesh-vimal/password-generator-react/",
+        },
+        {
+            name: "Pokemon App",
+            desc: "A website to display the pokemon details like abilities, type, gender etc.. It is build using <b> Reactjs</b>.",
+            image: pokemonApp,
+            website: "https://pranesh-pokemon.netlify.app/",
+            github: "https://github.com/Pranesh-vimal/pokemon-react-app/",
+        },
+        {
+            name: "Ecommerce",
+            desc: "A fully featured ecommerce web application with product management, order management and delivery management. It is developed using <b> Laravel </b> and <b> Livewire</b>.",
+            image: spot,
+            website: "#ecommerce",
+            github: "https://github.com/Pranesh-vimal/spot-ecommerce/",
+        },
+        {
+            name: "Ecommerce",
+            desc: "A fully featured ecommerce web application with product management, order management and delivery management. It is developed using <b> Nodejs</b>.",
+            image: royalcakes,
+            website: "https://royalcakes.in/",
+            github: "#ecommerce",
+        },
+        {
+            name: "Say About Me",
+            desc: "An anonymous for confession, it has OAuth for login and build using <b> Php</b>.",
+            image: sayaboutme,
+            website: "http://sayaboutme.ga/",
+            github: "https://github.com/Pranesh-vimal/SayAboutMe/",
+        },
+        {
+            name: "Movie Finder",
+            desc: "A movie finder application which displays posters of the movie. It is developed using <b> Reactjs</b>.",
+            image: omdb,
+            website: "https://pranesh-omdb.netlify.app/",
+            github: "https://github.com/Pranesh-vimal/OMDbAPI-React/",
+        },
+        {
+            name: "Tic Tac Toe",
+            desc: "A simple tic tac toe game build using <b> Reactjs</b>.",
+            image: tictactoe,
+            website: "https://pranesh-tik-tak-toe.netlify.app/",
+            github: "https://github.com/Pranesh-vimal/OMDbAPI-React/",
+        },
+    ];
+}
+
+export default works;
